f1dfc11b52bd8097482382f466120f8e65d37e80
sean-chou <sean_chou@trend.com.tw>
2021-03-18 21:36:56 +0800

    init project with CRA

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Control from './components/Control';
+import Content from './components/Content';
+
+const App = () => {
+    return (
+        <>
+            <Control />
+            <Content />
+        </>
+    );
+}
+
+export default App;
---
+++ app/src/components/Control.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Control = () => {
+    return (
+        <>
+            Control
+        </>
+    );
+};
+export default Control;
