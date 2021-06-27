10adfce83c96c42502e5bda928b88d72999dc783
Sean Chou <sean1093@gmail.com>
2021-06-27 17:26:10 +0800

    update todo baisc sample for extacting components

--- app/src/App.todo.basic.js
+++ app/src/App.todo.basic.js
@@ -1,31 +1,47 @@
 import React, { useState } from 'react';
 
-const App = () => {
-    const [list, setList] = useState([]);
+const TodoList = ({ list }) => (
+    <div>
+    {
+        list.map((item) => {
+            return <div>{item}</div>;
+        })
+    }
+    </div>
+);
+
+const Control = ({ onAdd }) => {
     const [value, setValue] = useState('');
-    
-    const add = () => {
-        const newArray = [...list, value];
-        setList(newArray);
+
+    const onChange = (e) => {
+        setValue(e.target.value);
+    }
+    const addItem = (e) => {
+        onAdd(value);
         setValue('');
     }
-    
-    const change = (e) => {
-        setValue(e.target.value)
-    }
-    
-    const renderList = list.map((item) => {
-        return <div>{item}</div>;
-    });
-    
+
     return (
-        <div>
-            <input value={value} onChange={change}/>
-            <button onClick={add}>Add</button>
-            <div>
-                {renderList}
-            </div>
-        </div>
+        <>
+            <input value={value} onChange={onChange}/>
+            <button onClick={addItem}>Add</button>
+        </>
+    );
+};
+
+const App = () => {
+    const [list, setList] = useState([]);
+
+    const onAdd = (value) => {
+        const newArray = [...list, value];
+        setList(newArray);
+    }
+
+    return (
+        <>
+            <Control onAdd={onAdd} />
+            <TodoList list={list} />
+        </>
     );
 }
 
